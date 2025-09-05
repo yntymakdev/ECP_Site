13278fe06e230658218b1776abd7d80dd7a87edd
yntymakdev <kubanychevyntymak21@gmail.com>
2025-09-05 18:59:53 +0600

    as

---
+++ src/constants/seo.constant.ts
@@ -0,0 +1,4 @@
+export const NO_INDEX_PAGE = { robots: { index: false, follow: false } };
+
+export const SITE_NAME = "ЕРЦ — все коммунальные услуги в одном приложении";
+export const SITE_DESCRIPTION = "Добавляйте объекты, подключайте услуги, оплачивайте счета в одном приложении";
