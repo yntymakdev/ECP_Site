6a8c89eaab0e6a54d22cd1a9cef4e6a1a13e7b7e
yntymakdev <kubanychevyntymak21@gmail.com>
2025-09-25 16:46:49 +0600

    as

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,6 +35,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ru">
+      <head>
+        {/* ✅ Добавляем сюда метатег для Facebook */}
+        <meta name="facebook-domain-verification" content="wu2ejviwbs2j8jqmb1r1bxqhwnhznf" />
+      </head>
       <body
         className={`${geistSans.variable} 
       ${geistMono.variable} antialiased`}
